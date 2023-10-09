f97a468bb770c3f6bd2720c1b43baab367ea8161
naumb <nauzetmartinbaez@outlook.es>
2023-10-09 18:52:49 +0100

    Exception handler improvements.

--- src/main/java/com/example/demo/exceptions/handlers/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/handlers/RestExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
@@ -20,7 +21,11 @@ import static java.text.MessageFormat.format;
 
 
 @ControllerAdvice
-public class TaskExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = { CreateTaskException.class, CreateTaskException.class })
+    protected ResponseEntity<Object> handleCreateTaskException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
     @ExceptionHandler(value = { TaskNotFoundException.class, TaskNotFoundException.class })
     protected ResponseEntity<Object> handleTaskNotFound(NoSuchElementException ex, WebRequest request) {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
@@ -31,25 +36,23 @@ public class TaskExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
-    @ExceptionHandler(value = { CreateTaskException.class, CreateTaskException.class })
-    protected ResponseEntity<Object> handleCreateTaskException(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    @ExceptionHandler(value = { MethodArgumentTypeMismatchException.class })
+    protected ResponseEntity<Object> handleInvalidArgument(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, format("Invalid argument: [{0}]", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
-    //Move to a generic handler with other generic exceptions.
     @ExceptionHandler(value = { ConversionFailedException.class, ConversionFailedException.class })
     public ResponseEntity<Object> handleConflict(ConversionException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(ex, format("Conversion failed: [{0}]", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
-    //Move to a generic handler with other generic exceptions.
     @ExceptionHandler(value = { PropertyReferenceException.class })
     protected ResponseEntity<Object> handleInvalidParam(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(ex, format("Invalid property specified: [{0}]", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(value = { Exception.class })
     protected ResponseEntity<Object> handleGeneralException(Exception ex, WebRequest request) {
-        return handleExceptionInternal(ex, format("Unexpected error. Reason: [{0}]", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(ex, format("Unexpected error: [{0}]", ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
