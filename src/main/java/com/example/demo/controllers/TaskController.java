bcbf0ab90b51f6c1f8d64395ee2073cde08a3b66
naumb <nauzetmartinbaez@outlook.es>
2023-10-09 18:58:54 +0100

    Task controller improvements.

--- src/main/java/com/example/demo/controllers/TaskController.java
+++ src/main/java/com/example/demo/controllers/TaskController.java
@@ -25,26 +25,26 @@ public class TaskController {
             @RequestParam(required = false) Boolean completed,
             @RequestParam(required = false, defaultValue = "creationDate") String sortField,
             @RequestParam(required = false, defaultValue = "DESC") Direction sortDirection) {
-        return new ResponseEntity<>(taskService.getAllTasks(priority, completed, sortField, sortDirection), HttpStatus.OK);
+        return new ResponseEntity(taskService.getAllTasks(priority, completed, sortField, sortDirection), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
-        return new ResponseEntity<>(taskService.getTaskById(id), HttpStatus.OK);
+        return new ResponseEntity(taskService.getTaskById(id), HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public ResponseEntity createTask(@RequestBody TaskDTO taskDTO) {
-        return new ResponseEntity<>(taskService.createTask(taskDTO), HttpStatus.CREATED);
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+        return new ResponseEntity(taskService.createTask(taskDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
-    public ResponseEntity updateTask(@RequestBody TaskDTO taskDTO) {
-        return new ResponseEntity<>(taskService.updateTask(taskDTO), HttpStatus.OK);
+    public ResponseEntity<TaskDTO> updateTask(@RequestBody TaskDTO taskDTO) {
+        return new ResponseEntity(taskService.updateTask(taskDTO), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity deleteTask(@PathVariable Long id) {
-        return new ResponseEntity<>(taskService.deteleTask(id), HttpStatus.OK);
+        return new ResponseEntity(taskService.deteleTask(id), HttpStatus.OK);
     }
 }
